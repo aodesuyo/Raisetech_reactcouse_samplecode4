76539ce961c1e5c6eb40b9b8864333dcee736aa0
aodesuyo <64908452+aodesuyo@users.noreply.github.com>
2021-11-18 11:06:00 +0900

    DOM操作

--- src/09dom.js
+++ src/09dom.js
@@ -4,7 +4,7 @@ const contentsitem = document.getElementsByClassName("content");
 console.log("タイトル要素", title);
 console.log("コンテンツ要素", contentsitem);
 
-//DOMの作成・追加
+//DOMの作成
 
 //入れるアイテムを作成する
 //createElementでdiv要素を作成する変数を宣言
@@ -16,3 +16,14 @@ content.innerHTML = "<p>SPAとは</p>";
 const contents = document.getElementById("contents");
 //読み込んだ中にアイテムを差し込む
 contents.appendChild(content);
+
+//DOMの削除
+const deletecontent = document.createElement("div");
+const deletecontentclass = deletecontent;
+deletecontentclass.classList.add("deleteclass");
+deletecontentclass.setAttribute("id", "deleteid");
+deletecontent.innerHTML = "<p>jsで追加した要素だからか取得できない？</p>";
+
+contents.appendChild(deletecontent);
+const deleteid = document.getElementById("content");
+deleteid.remove();
